85a889fe2221fe6611e55cde3a778eae36ef80bf
Hyunu Jung <hyunujung0419@gmail.com>
2022-02-08 13:21:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 let rawdata = fs.readFileSync('config.json');
 let config = JSON.parse(rawdata);
 const token = config.token;
+const prefix = '>';
 
 for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
@@ -20,7 +21,26 @@ client.once('ready', () => {
         console.log('Kidingnyan is Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-})
+client.on('interactionCreate', async interaction => {});
+client.on('messageCreate', function(message) {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+
+        const commandBody = message.content.slice(prefix.length);
+        const args = commandBody.split(' ');
+        const command = args[2].shift().toLowerCase();
+
+        console.log(args);
+
+        if (command === 'ping') {
+                const timeTaken = Date.now() - message.createdTimestamp;
+                message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+        }
+});
+client.on('message', message => {
+        if (client.message === '>hello') {
+                message.reply('Hello!');
+        }
+});
 //로그인
 client.login(token);
