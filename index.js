51c9eae7d623bc3efbfa03410c981f1766cc51ee
chrispark0125 <chrispark0125@github.com>
2022-02-09 12:30:24 +0900

    Msilot Fix 1

--- index.js
+++ index.js
@@ -2,45 +2,51 @@ const { User } = require("./User.js");
 var db = require('quick.db');
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [ // Intent를 설정합니다. 설정하지 않으면 CLIENT_MISSING_INTENTS 오류가 발생합니다.
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES 
+] });
 client.commands = new Collection();
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-let rawdata = fs.readFileSync('config.json');
-let config = JSON.parse(rawdata);
+let config = require('./config.json')
 const token = config.token;
-const prefix = '>';
+const prefix = '>'
 
+//커맨드 로딩
 for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
-        client.commands.set(command.data.name, command);
+        client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
         console.log('Kidingnyan is Ready!');
 });
 
-client.on('interactionCreate', async interaction => {});
-client.on('messageCreate', function(message) {
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
+client.on("messageCreate", async msg => {
+    // message 작성자가 봇이면 그냥 return
+    if (msg.author.bot) return;
+    // message 시작이 prefix가 아니면 return
+    if (!msg.content.startsWith(prefix)) return;
 
-        const commandBody = message.content.slice(prefix.length);
-        const args = commandBody.split(' ');
-        const command = args[2].shift().toLowerCase();
+    const commandBody = msg.content.slice(prefix.length);
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const commandtext = args.shift().toLowerCase();
 
-        console.log(args);
+    console.log(`L37 ${args}`)
 
-        if (command === 'ping') {
-                const timeTaken = Date.now() - message.createdTimestamp;
-                message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-        }
+    const command = client.commands.get(commandtext);
+
+    //없으면 리턴
+    if (!command) return;
+
+    //디버깅
+    console.log(`${msg.member.user.username}#${msg.member.user.discriminator} Messaged \"${msg.content}\"`)
+
+    //execute
+    await command.execute(client, msg);
 });
-client.on('message', message => {
-        if (client.message === '>hello') {
-                message.reply('Hello!');
-        }
-});
+
 //로그인
 client.login(token);
